6672add6d31a3c56aa387add0b4949dcb395dc74
ExoticDuck <marchenkov.n@inbox.ru>
2022-05-18 19:48:52 +0300

    fixes

--- src/Redux/UserReducer.ts
+++ src/Redux/UserReducer.ts
@@ -1,4 +1,4 @@
-import { AxiosError, AxiosResponse } from "axios";
+import { AxiosError } from "axios";
 import { Dispatch } from "redux";
 import { UserAPI } from "../API/api";
 import { setUserFound, setUserIsFetching } from "./AppReducer";
